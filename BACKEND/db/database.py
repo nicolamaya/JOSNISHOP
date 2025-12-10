997a0020a91ff6a498dc2ad47f2110e02c0b40cf
JosthinPaz <josthinpaz2@gmail.com>
2025-12-10 08:59:25 -0500

    fix: main.yml y database.py

--- BACKEND/db/database.py
+++ BACKEND/db/database.py
@@ -1,5 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+DB_HOST = os.getenv("DB_HOST", "localhost")  # Predeterminado para desarrollo
+DB_PORT = os.getenv("DB_PORT", "3315")        # Predeterminado para desarrollo
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASS = os.getenv("DB_PASS", "admin")
+DB_NAME = os.getenv("MYSQL_DATABASE", "josnishop") # Nombre de la BD
 
 # connection string
 # represenat la base de datos a conectar
