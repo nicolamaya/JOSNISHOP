06548aaca77b1e82eec623119c3ca21305ba5117
JosthinPaz <josthinpaz2@gmail.com>
2025-12-11 07:20:48 -0500

    fix: database.py

--- BACKEND/db/database.py
+++ BACKEND/db/database.py
@@ -11,7 +11,7 @@ DB_NAME = os.getenv("MYSQL_DATABASE", "josnishop") # Nombre de la BD
 # connection string
 # represenat la base de datos a conectar
 # dependiendo la base de datos que se use y el lenguaje de programación
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:admin@localhost:3315/josnishop"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # crea el objeto de conexion (permite conectarse a la base de datos)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
