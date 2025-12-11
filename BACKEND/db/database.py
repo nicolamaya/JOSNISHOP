75ab450d53282e8dc7af4394fbdcaa73baad4564
JosthinPaz <josthinpaz2@gmail.com>
2025-12-11 07:59:38 -0500

    feat: Archivos Adicionales Backend

--- BACKEND/db/database.py
+++ BACKEND/db/database.py
@@ -1,17 +1,10 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-DB_HOST = os.getenv("DB_HOST", "localhost")  # Predeterminado para desarrollo
-DB_PORT = os.getenv("DB_PORT", "3315")        # Predeterminado para desarrollo
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASS = os.getenv("DB_PASS", "admin")
-DB_NAME = os.getenv("MYSQL_DATABASE", "josnishop") # Nombre de la BD
 
 # connection string
 # represenat la base de datos a conectar
 # dependiendo la base de datos que se use y el lenguaje de programación
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:admin@localhost:3315/josnishop"
 
 # crea el objeto de conexion (permite conectarse a la base de datos)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
