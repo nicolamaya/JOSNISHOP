4b48a5df7be434abeb06284383307cee85f8f552
JosthinPaz <josthinpaz2@gmail.com>
2025-12-11 08:43:26 -0500

    fix: main.yml y database.py

--- BACKEND/db/database.py
+++ BACKEND/db/database.py
@@ -1,10 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# connection string
-# represenat la base de datos a conectar
-# dependiendo la base de datos que se use y el lenguaje de programación
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:admin@localhost:3315/josnishop"
+# --- Nueva configuración: Leer variables de entorno ---
+DB_USER = os.getenv("DB_USER", "root")          # Usamos 'root' como fallback, aunque en CI es vacío
+DB_PASSWORD = os.getenv("DB_PASSWORD", "admin") # Usamos 'admin' como fallback, aunque en CI es vacío
+DB_HOST = os.getenv("DB_HOST", "localhost")     # Lee el host. Fallback a 'localhost' para desarrollo local.
+DB_PORT = os.getenv("DB_PORT", "3315")          # Lee el puerto. Fallback a '3315' para desarrollo local.
+DB_NAME = os.getenv("DB_NAME", "josnishop")     # Lee el nombre de la DB. Fallback a 'josnishop'
+
+# connection string (usando variables de entorno)
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)   # Lee el nombre de la DB. Fallback a 'josnishop'
 
 # crea el objeto de conexion (permite conectarse a la base de datos)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
